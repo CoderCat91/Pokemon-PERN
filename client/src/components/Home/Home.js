b98b4472a11a743fd916e381f9edaff6b70d05ca
Chelsey Duffin <Chelseyrd91@hotmail.com>
2023-09-13 19:59:04 +0100

    Added pokemon detail cards

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import Header from "../Header/Header"
 import SearchBar from "../SearchBar/SearchBar"
 import './Home.scss';
+import PokemonFinder from '../../api/PokemonFinder';
 import SearchResultsList from '../SearchBar/SearchResultsList';
+import { useNavigate } from 'react-router-dom';
+import { PokemonContext } from '../../context/PokemonContext';
 
 const Home = () => {
   const [results, setResults] = useState([]);
+  const { pokemons, setPokemons } = useContext(PokemonContext);
+const navigate = useNavigate();
+
+useEffect(() => {
+        (async () => {
+          try {
+            const response = await PokemonFinder.get("/");
+            console.log(response.data.data.pokemon);
+            setPokemons(response.data.data.pokemon)
+          } catch (err) {
+            console.log(err);
+          }
+        })();  // making sure you run the function, not just defining it!
+      }, [setPokemons]);
+
+const fireSelect = () => {
+  var fireType =  pokemons.filter(function(pokemon) {
+    return pokemon.type === "Fire";
+  });
+  
+  console.log(fireType);
+}    
+  
+
+      const selectPokemon = (id) => {
+        navigate(`/details/${id}`);
+      };
+
   return (
     <div>
         <Header/>
@@ -20,6 +51,45 @@ const Home = () => {
          Use the PokeDex to learn about your favourite pocket monsters, then add them to your own personal PokeDex!
           </div>
         </div>
+        <div className="list-group container">
+        <table className="table table-hover table-dark">
+          <thead>
+            <tr className="bg-primary">
+            <th scope="col">PokeDex No.</th>
+            <th scope ='col'>Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Type</th>
+              <th scope="col">Health</th>
+              <th scope="col">Attacks</th>
+              <th scope="col">Evolves into:</th>
+              <th scope='col'>Rating</th>
+              <th scope="col">Profile</th>
+            </tr>
+          </thead>
+          <tbody>
+          {pokemons && pokemons
+             .sort(({ id: previousID }, { id: currentID }) => previousID - currentID)
+             .map((pokemon) => {
+
+        return (
+   <tr    key={pokemon.id}>
+
+<td>{pokemon.pokemon_num}</td>
+   <td className='pokedex-images'><img src={pokemon.images} alt='pokemon'/></td>
+   <td>{pokemon.name}</td>
+   <td>{pokemon.type}</td>
+   <td>{pokemon.health}</td>
+   <td>{pokemon.attacks}</td>
+   <td>{pokemon.evolves_into}</td>
+   <td>rating</td>
+   <td><button onClick={() => selectPokemon(pokemon.id)}className='btn btn-danger'>Profile</button></td>
+</tr>
+                  )
+                })}
+    
+          </tbody>
+        </table>
+      </div>
     </div>
 
   )
--- client/src/components/PokemonDetails/PokemonDetails.js
+++ client/src/components/PokemonDetails/PokemonDetails.js
@@ -1,9 +1,14 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { PokemonContext } from '../../context/PokemonContext';
 import PokemonFinder from '../../api/PokemonFinder';
 import { useParams } from 'react-router-dom';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResultsList from '../SearchBar/SearchResultsList';
+import './PokemonDetails.scss';
+
 const PokemonDetails = () => {
+  const [results, setResults] = useState([]);
     const { id } = useParams();
     const { selectedPokemon, setSelectedPokemon } = useContext(
       PokemonContext
@@ -26,14 +31,35 @@ const PokemonDetails = () => {
     return (
       <div>
         <Header/>
+        <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
         {selectedPokemon && (
-          <>
-            <h1 className="text-center display-1">
-              {selectedPokemon.name}
-            </h1>
-            <div className="text-center">
+          <div className="detail-wrapper">
+            <div className="detail-card">
+              <div className='card-inner'>
+              <div className='card-top'>
+                #{selectedPokemon.pokemon_num}<br/>
+             <h2>{selectedPokemon.name}</h2> 
+              <div className='detail-image'>
+              <img src={selectedPokemon.images} alt="pokemon"/>
             </div>
-          </>
+            </div>
+        
+            <div className="card-middle">
+            Type: {selectedPokemon.type}<br/>
+              Health: {selectedPokemon.health} HP<br/>
+              Attacks: {selectedPokemon.attacks}<br/>
+              Evolves into: {selectedPokemon.evolves_into}
+            </div>
+            <div className='card-bottom'>
+            <button className='btn btn-danger'>Previous</button>
+            <button className='btn btn-danger'>Next</button>
+              </div>
+              </div>
+          </div>
+          </div>
         )}
       </div>
     );
