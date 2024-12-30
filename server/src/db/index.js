ac0452b39b48bf00aaff906d7791f55aebf038bf
Chelsey Duffin <Chelseyrd91@hotmail.com>
2024-12-30 20:49:32 +0000

    .

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -6,18 +6,5 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },          
 });
 
-async function testConnection() {
-  try {
-    const client = await pool.connect();
-    console.log('Successfully connected to the database!');
-    client.release();
-  } catch (err) {
-    console.error('Database connection error:', err.stack);
-  }
-}
 
-testConnection();
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
