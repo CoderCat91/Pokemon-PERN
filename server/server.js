0e24437fd53599c1e77bbc3c21c3ab1264e4dc49
Chelsey Duffin <Chelseyrd91@hotmail.com>
2024-12-30 22:22:07 +0000

    Auth bug

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(passport.initialize())
 const corsOptions = {
   origin: 'https://pokemon-pern-app.onrender.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,  
 };
 
