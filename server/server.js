495d1aa043477cb45c926952acc8b4d4f2e2a2a8
Chelsey Duffin <Chelseyrd91@hotmail.com>
2024-12-30 21:33:55 +0000

    debug

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { DATABASE_URL} = require('./src/DB/index')
+const { DATABASE_URL} = require('./src/db/index')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
 const cors = require('cors')
