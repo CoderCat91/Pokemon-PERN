7275d178a649a90c7a34ef2311b95efa59578871
Chelsey Duffin <Chelseyrd91@hotmail.com>
2025-01-07 17:38:54 +0000

    Added paragraphs, altered logout button

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -21,12 +21,13 @@ const Home = () => {
           <Row>
             <Col xl={12}>
         <div className="home-inner">
-         <p className='home-intro'>Everyone remembers the 1996 epic creation that is Pokémon. Whether you collected the cards, watched the shows or played the games, Pokémon was probably part of your childhood. Our Pokémon page features the original 151 Pokémon!
+         <p className='home-intro'>Everyone remembers the 1996 epic creation that is Pokémon. Whether you collected the cards, watched the shows or played the games, Pokémon was probably part of your childhood.
+         <br/>Our Pokémon page features the original 151 Pokémon!
          Use the Pokémon page to add your favourite Pokémon to your own personal Pokédex to learn more about your favourite pocket monsters!
          </p>
          <Container fluid className="home-media">
         <Row className="home-row">
-          <Col xl ={4}>
+          <Col xl ={4} sm={12}>
           <Card className="ven-fig">
             <Card.Img src={venasaur} alt="Venasaur"/>
             <Card.Title>
@@ -34,7 +35,7 @@ const Home = () => {
             </Card.Title>
           </Card>
           </Col>
-          <Col xl={8} className='tcg-col'>
+          <Col xl={8} sm={12}className='tcg-col'>
             <div className="tcg-card">
               <p> First launched in 1996, the TCG allows players to collect, trade, and build decks featuring their favorite Pokémon. Each card includes unique artwork, stats, and abilities, making collecting them as rewarding as battling them with other players. The Pokémon TCG has also become a valuable venture, with rare cards like a shiny Charizard and special edition sets fetching high prices and admiration. The original 1996 complete sets such as the Jungle set have been sold at auction for many thousands of dollars, especially those that contain full shiny editions. </p>
             </div>
@@ -42,7 +43,7 @@ const Home = () => {
           </Row>
           <Row className="home-row">
           
-            <Col xl={4}>
+            <Col xl={4} sm={6}>
             <Card className="char-fig">
           <Card.Img src={charizard} alt="Charizard"/>
             <Card.Title>
@@ -50,17 +51,17 @@ const Home = () => {
             </Card.Title>
           </Card>
             </Col>
-            <Col xl={8} className='movies-col'>
+            <Col xl={8} sm={6} className='movies-col'>
           <div className="movies-card">
              <p>Starting in 1997, the first season of the TV series Pokémon hit screens in Tokyo, with the anime being dubbed in English soon afterwards. 
-              Fans loved the humour, adventurous story lines and following the main protagonist Ash Ketchum while he starts his journey to become a Pokémon master. 
-              His journey takes many exciting twists and turns, which captures the attention of Jesse, James and Meowth also known as Team Rocket. The trio of Pokémon theives become obsessed with capturing Ash's Pikachu, but his faithful friends Brock and Misty help Ash to defeat Team Rocket and save countless Pokémon from being captured. The shows were extremely successful internationally with a movie being released in 1998 Pokémon: The First Movie. The film takes place during the first season of the show, Indigo League and introduces two new Pokémon, Mew and MewTwo. 
+              Fans loved the humour, adventurous story lines and following the main protagonist Ash Ketchum while he starts his journey to become a Pokémon master. <br/>
+              His journey takes many exciting twists and turns, which captures the attention of Jesse, James and Meowth also known as Team Rocket.<br/> The trio of Pokémon theives become obsessed with capturing Ash's Pikachu, but his faithful friends Brock and Misty help Ash to defeat Team Rocket and save countless Pokémon from being captured. The shows were extremely successful internationally with a movie being released in 1998 Pokémon: The First Movie. The film takes place during the first season of the show, Indigo League and introduces two new Pokémon, Mew and MewTwo. 
               </p>
           </div>
           </Col>
           </Row>
           <Row className="home-row">
-            <Col xl={4}>
+            <Col xl={4} sm={6}>
           <Card className="blas-fig">
             <Card.Img src={blastoise} alt="Blastoise" />
             <Card.Title>
@@ -68,10 +69,10 @@ const Home = () => {
             </Card.Title>
           </Card>
           </Col>
-          <Col xl={8} className='games-col'>
+          <Col xl={8} sm={6} className='games-col'>
           <div className="games-card">
-            <p>Pokémon Red and Pokémon Blue were released in 1996, kicking off one of the most successful gaming series of all time, selling 300 million copies world wide. They were quickly follow by Pokémon Yellow in 1998. The games were developed solely for the GameBoy, a handheld device that accepts cartridges as a software component.
-              The games were well recieved by critics and have a cult following. Pokémon Gold and Silver were later released in 1999, with Pokémon Crystal launching in 2000. The most recent games Pokémon Scarlett and Violet were released in 2022, highlighting the longevity and success of the franchise and how Pokémon has engaged multiple generations since childhood.</p>
+            <p>Pokémon Red and Pokémon Blue were released in 1996, kicking off one of the most successful gaming series of all time, selling 300 million copies world wide. They were quickly follow by Pokémon Yellow in 1998. <br/> The games were developed solely for the GameBoy, a handheld device that accepts cartridges as a software component.
+              The games were well recieved by critics and have a cult following. Pokémon Gold and Silver were later released in 1999, with Pokémon Crystal launching in 2000. <br/> The most recent games Pokémon Scarlett and Violet were released in 2022, highlighting the longevity and success of the franchise and how Pokémon has engaged multiple generations since childhood.</p>
           </div>
           </Col>
           </Row>
